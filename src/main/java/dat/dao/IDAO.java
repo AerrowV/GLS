b153a3dc5257256b4ffe1bc8b685f6f6a832662e
Jonas <glemdeet@gmail.com>
2025-02-13 12:42:21 +0100

    IDAO

--- src/main/java/dat/dao/IDAO.java
+++ src/main/java/dat/dao/IDAO.java
@@ -1,4 +1,11 @@
 package dat.dao;
 
-public interface IDAO {
+import java.util.List;
+
+public interface IDAO<T,I> {
+    T create(T t);
+    T read(I i);
+    List<T> readAll();
+    T update(T t);
+    void delete(I i);
 }
